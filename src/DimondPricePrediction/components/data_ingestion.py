aefbe277903befb209b91dfed7febbf2e292e472
omshivasai <shivasaigurrapu@gmail.com>
2024-02-09 18:13:05 +0530

    updated logger and exception

---
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -0,0 +1,8 @@
+
+
+class DataIngestion(self):
+    def __init__(self):
+        pass
+
+    def initiate_data_ingestion(self):
+        pass
---
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -0,0 +1,10 @@
+
+
+
+
+class DataIngestion:
+    def __init__(self):
+        pass
+
+    def initiate_nodel_training(self):
+        pass
